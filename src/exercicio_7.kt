f1d6d17f6d6d827ad347dba70a947551fd480a35
Joice Fonseca <joicemfonseca@hotmail.com>
2020-03-24 22:14:09 -0300

    exercicio 7

---
+++ src/exercicio_7.kt
@@ -0,0 +1,19 @@
+fun main() {
+    println("Em qual período você estuda?")
+    val valor = readLine()!!.toString()
+
+    val matutino = valor == "M"
+    val vespertino = valor == "V"
+    val noturno = valor == "N"
+
+    if (matutino) {
+        println("Bom dia")
+    } else if (vespertino) {
+        println("Boa tarde")
+
+    } else if (noturno) {
+        println("Boa noite")
+    } else {
+        println("Valor inválido")
+    }
+}
